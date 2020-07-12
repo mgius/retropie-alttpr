92e53305ce134c4e91f5bf4be295e2f639c22614
Mark Gius <mgius7096@gmail.com>
2020-07-11 20:34:25 -0700

    this works with a special branch of pyz3r

--- retropie_alttpr/api.py
+++ retropie_alttpr/api.py
@@ -9,6 +9,8 @@ from retropie_alttpr import cache, util
 
 from loguru import logger
 import pyz3r
+import pyz3r.http
+import pyz3r.alttpr
 
 
 @cache.misc.transparent_cache('randomizer_settings')
@@ -23,33 +25,42 @@ async def fetch_randomizer_settings():
     return all_settings
 
 
+async def fetch_base_patch(site: pyz3r.http.site, patch_hash):
+    return await site.retrieve_binary(f"/bps/{patch_hash}.bps")
+
+
 @util.async_to_sync
-async def _generate_rom(settings, base_rom_filename, patched_rom_filename):
-    logger.debug(f"Generating ROM with settings {settings}", enqueue=True)
+async def generate_seed(settings: dict):
     seed = await pyz3r.alttpr(
         settings=settings
     )
 
-    base_rom = await pyz3r.rom.read(base_rom_filename)
+    base_patch_hash = seed.data['current_rom_hash']
 
-    patched_rom = await seed.create_patched_game(base_rom)
-
-    await pyz3r.rom.write(patched_rom, patched_rom_filename)
+    if not cache.base_patch.exists(base_patch_hash):
+        base_patch = await fetch_base_patch(seed.site, base_patch_hash)
+        cache.base_patch.put(base_patch_hash, base_patch)
 
     logger.info(f"Generated Hash: {seed.hash}")
     logger.info(f"Generated Code: {seed.code}")
 
-
-def generate_rom(settings: dict) -> bytes:
-    base_rom_data = cache.misc.get('base_zelda_rom')
-    with util.in_memory_tempfile() as base_rom_file, util.in_memory_tempfile() as patched_rom_file:
-        base_rom_file.write(base_rom_data)
-        base_rom_file.flush()
-        _generate_rom(settings, base_rom_file.name, patched_rom_file.name)
-
-        return base_rom_file.read()
+    return seed
 
 
 @util.async_to_sync
 async def validate_rom(rom_filename):
     await pyz3r.rom.read(rom_filename)
+
+
+@util.async_to_sync
+async def generate_rom(settings: dict):
+    import pdb; pdb.set_trace()
+    seed = await pyz3r.alttpr(
+        settings=settings
+    )
+
+    base_rom = list(cache.misc.get('base_zelda_rom'))
+
+    patched_rom = await seed.create_patched_game(base_rom)
+
+    return bytes(patched_rom)
--- scripts/generate.py
+++ scripts/generate.py
@@ -1,20 +1,64 @@
-import asyncio
+import json
+import os
 
-import click
+import pyz3r.patch
+from dialog import Dialog
 
-from retropie_alttpr import api, util
+from retropie_alttpr import api, cache, randomizer_config, util
 
 
-@click.command()
-@click.option("--base-rom-filename", type=str, required=True)
-@click.option("--patched-rom-filename", type=str, required=True)
-@click.option("--preset", type=str, required=True)
-def main(base_rom_filename, patched_rom_filename, preset):
-    if preset is not None:
-        settings = asyncio.run(fetch_preset(preset))
+class GenerateMenu:
+    def __init__(self):
+        self.dialog = Dialog(autowidgetsize=True)
 
-    asyncio.run(
-        generate_rom(settings, base_rom_filename, patched_rom_filename))
+    def patch_from_seed(self, seed_data: dict) -> bytes:
+        # pyz3r wants a list of bytes
+        base_rom = list(cache.misc.get('base_zelda_rom'))
+        base_patch = cache.base_patch.get(seed_data['current_rom_hash'])
+        randomizer_patch = seed_data['patch']
+
+        base_patched_rom = pyz3r.patch.apply_bps(base_rom, base_patch)
+        extended_rom = pyz3r.patch.expand(base_patched_rom, seed_data['size'])
+        randomized_rom = pyz3r.patch.apply(extended_rom, randomizer_patch)
+        final_rom = pyz3r.patch.apply(extended_rom, pyz3r.patch.checksum(randomized_rom))
+
+        return bytes(final_rom)
+
+    def get_seed_for_config_name(self, config_name):
+        loaded_config = json.loads(cache.config.get(config_name))
+        seed_config = randomizer_config.to_seed_gen_dict(
+                randomizer_config.from_preset_dict(loaded_config))
+
+        return api.generate_seed(settings=seed_config)
+
+    def driver(self):
+        choices = [(c, c) for c in ["alpha", "beta", "gamma", "delta"]]
+
+        code, tag = self.dialog.menu("Choose a config to generate and execute", choices=choices)
+
+        if code in (self.dialog.ESC, self.dialog.CANCEL):
+            return
+
+        if code != self.dialog.OK:
+            raise Exception(f"code == {code}")
+
+        loaded_config = json.loads(cache.config.get(tag))
+        seed_config = randomizer_config.from_preset_dict(loaded_config)
+        seed_config_dict = randomizer_config.to_seed_gen_dict(seed_config)
+        rom = api.generate_rom(seed_config_dict)
+
+        write_to = util.in_memory_tempfile(delete=False)
+        write_to.write(rom)
+        write_to.close()
+
+        os.execv("/opt/retropie/supplementary/runcommand/runcommand.sh",
+                 "0", "_SYS_", "snes", write_to.name)
+
+        raise Exception("Failed to Exec")
+
+
+def main():
+    GenerateMenu().driver()
 
 
 if __name__ == '__main__':
